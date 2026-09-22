7435d6a6e9a40608ea2a55c697cc525c4f7def8d
Ryan <ryanalves1100@gmail.com>
2026-09-22 17:39:50 -0300

    implementing enum

--- src/main/java/com/devryan/course/entities/Order.java
+++ src/main/java/com/devryan/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.devryan.course.entities;
 
+import com.devryan.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -17,6 +18,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT") //anotação jackson que serve para ditar como um campo json deve ser formatado
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne //anotação JPA que mapeia relacionamento "Muitos para um", muitos registros dessa classe podem estar associados a uma outra classe
     @JoinColumn(name = "client_id") // serve para nomear o nome da coluna onde a chave estrangeira será armazenada
     private User client;
@@ -24,10 +27,11 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -52,6 +56,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
